1459adc99c22e0698e4ba29a5f176fd7adbac0af
daniela-wiepert <58085556+daniela-wiepert@users.noreply.github.com>
2019-11-27 13:23:18 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,37 @@
-import tweepy
+import numpy as np
+import tensorflow as tf
+import csv
+def get_data(train_file,test_file):
+    train_file = 'data/train.csv'
+    test_file = 'data/test.csv'
+    
+    #for comments - figure out how to remove the \\xja;lskdjf; @symbols etc. anything that isn't a regular letter and isn't a word
+    #first split on spaces? unk any word that contains a non-english letter??
+    #create a vocab dict that maps each word in the corpus to a unique index (its id)
+    # TODO: read in and tokenize training data
+    # convert to their indices (making a 1-d list/array)
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-api = tweepy.API(auth)
+    # TODO: read in and tokenize testing data
+    # convert to their indices (making a 1-d list/array)
 
-tweet = api.get_status(572342978255048705)
-print(tweet.text)
+    # TODO: return tuple of training tokens, testing tokens, and the vocab dictionary.
+
+    #Do we need to pad the data? How are we tokenizing? How do we maintain the labels while tokenizing
+
+
+    #READ IN FILES
+    train_data = []
+    train_labels = []
+    with open(train_file) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            train_labels.append(row[0])
+            train_data.append(row[2])
+
+    test_data = []
+    test_labels = []
+    with open(test_file) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            test_labels.append(row[0])
+            test_data.append(row[2])

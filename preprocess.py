cca7c10fb16261e3a72f2b28c9ed838c0113df90
daniela-wiepert <58085556+daniela-wiepert@users.noreply.github.com>
2019-11-27 22:27:13 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,9 @@
 import csv
 import string
+from gensim.parsing.preprocessing import STOPWORDS
+import numpy as np
+
+#TODO: add comments to each function
 
 def isEnglish(s):
     #data coming in with unicode?
@@ -29,6 +33,18 @@ def removeSymbols(s):
                 tokens.append(token)
     return " ".join(tokens)
 
+def tokenize(s):
+    #TODO: tokenize the data - how are we doing this? by words, by characters?
+    words = s.split()
+
+    #TODO: remove stop words - filler words from gensim.parsing.preprocessing
+        #if you're running into an error, make sure that  gensim is installed (pip install -U gensim)
+    words = [word for word in words if word not in STOPWORDS]
+    return words
+
+def convert_to_id(vocab,sentences):
+    return np.stack([[vocab[word] if word in vocab else vocab[UNK_TOKEN] for word in sentence] for sentence in sentences])
+
 def get_data(train_file,test_file):
     #TODO: Figure out what the data needs to look like when it leaves preprocessing
     #COMMENT THESE OUT WHEN OFFICIALLY RUNNING
@@ -45,7 +61,9 @@ def get_data(train_file,test_file):
         for row in csvReader:
             if isEnglish(row[2]): #Check if comment is in English
                 train_labels.append(row[0])
-                train_data.append(removeSymbols(row[2])) #Remove symbols from the data, usernames, numbers, and links
+                s = removeSymbols(row[2]) #Remove symbols from the data, usernames, numbers, and links
+                words = tokenize(s.lower()) #make sure everything is lowercase and tokenize
+                train_data.append(words)
 
     test_data = []
     test_labels = []
@@ -54,15 +72,39 @@ def get_data(train_file,test_file):
         for row in csvReader:
             if isEnglish(row[2]): #Check if comment is in English
                 test_labels.append(row[0])
-                test_data.append(removeSymbols(row[2])) #Remove symbols from the data, usernames, numbers, and links
+                s = removeSymbols(row[2]) #Remove symbols from the data, usernames, numbers, and links
+                words = tokenize(s.lower()) #make sure everything is lowercase and tokenize
+                test_data.append(words)
 
+    #CHECKING THAT DATA HAS BEEN APPENDED CORRECTLY
+    print(train_data[0:3])
+    print(test_data[0:3])
 
-
-
-    #TODO: tokenize the data - how are we doing this? by words, by characters?
-
-    #TODO: pad data? is this necessary
-
-    #TODO: build vocab
+    #TODO: build vocab from train data
+    vocab = {}
+    reverse_vocab = {} #why is this necessary?
+    frequency = defaultdict(int) #why do we need frequency?
+    vocab_ind = 1
+    for data in train_data:
+        for word in data:
+            if word not in vocab:
+                vocab[word] = vocab_ind
+                reverse_vocab[vocab_ind] = word
+                vocab_ind +=1
+            freq[word] += 1
+    vocab['UNK'] = len(vocab) + 1
+    reverse_vocab[len(vocab)] = 'UNK' #why isn't it len(vocab) + 1 like the vocab?
 
     #TODO: convert data to their indices
+    #TODO: check that this is converting to IDs correctly
+    #What to do for test data, if word isn't in vocab, UNK it?
+    #What words should we UNK?
+    #Does this convert_to_id handle UNKING things?
+    train_data = convert_to_id(vocab,train_data)
+    test_data = convert_to_id(vocab,test_data)
+
+    #TODO: see what filter_vocab does?
+    #What to do for test data, if word isn't in vocab, UNK it?
+    #What words should we UNK?
+
+    #TODO: go through paper again and see how they are doing preprocessing/preparing the ngrams?
